fc0a962d5256c55986a4dc8ed49732065ee7831a
Thuong To <6074@ait.nsw.edu.au>
2018-08-23 00:04:10 +0000

    classes,images,includes

---
+++ includes/banner.php
@@ -0,0 +1,36 @@
+x<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/banner/vodkaoriginal.jpg" alt="Los Angeles"  width="1920" height="840">
+      <div class="carousel-caption">
+        <h3>ABSOLUT VODKA</h3>
+        <p>Discover all flavours of one of the most well-known vodkas in the world</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/banner/absolut-rainbow.jpg" alt="Chicago"  width="1920" height="840">
+      <div class="carousel-caption">
+        <h3>ABSOLUT RAINBOW</h3>
+        <p>Taking pride in diversity!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/banner/cocktails.jpg" alt="New York" width="1920" height="840">
+      <div class="carousel-caption">
+        <h3>COCKTAILS</h3>
+        <p>Discover the best cocktails and drink recipes</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+<!--<div class="footer">-->
+    
+    
+<!--</div>-->
+
+<footer>
+  <div class="footer">
+  <h3>Absolut. </h3>
+  <p> Copyright © 2018 Absolut. All rights reserved. </p>
+</div>
+
+</footer>
